7bcb92c1b8c11dc7826eab7c6e049fba6fa5704b
unknown <khmammed@gmail.com>
2019-05-12 17:27:02 +0000

    Allow extern request

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ mongoose.connect(uri, { useNewUrlParser: true})
 
 
 //Enable CORS For Extern WebSite
-app.use((req, res, next)=> {
+App.use((req, res, next)=> {
  res.header("Access-Control-Allow-Origin", "*");
  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
  next();
