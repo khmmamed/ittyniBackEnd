0d8accc80c4f1e07beaaeb7a5ec4d1193c78a91b
unknown <khmammed@gmail.com>
2019-05-12 17:25:42 +0000

    Allow extern request

--- App.js
+++ App.js
@@ -8,10 +8,20 @@ const  Port =  8000;
 mongoose.Promise = global.Promise;
 mongoose.set('useCreateIndex', true);
 
-const uri = "mongodb://localhost:27017/MPAPP";
+//const uri = "mongodb://localhost:27017/MPAPP";
+var uri = "mongodb+srv://khmamed:Projetdeweb1@cluster0-lhitx.gcp.mongodb.net/mpapp?retryWrites=true";
+mongoose.connect(uri, { useNewUrlParser: true})
+        .then(() => { console.log('MongoDB connected...')})
+        .catch(err => console.log(err));
 
 
 
+//Enable CORS For Extern WebSite
+app.use((req, res, next)=> {
+ res.header("Access-Control-Allow-Origin", "*");
+ res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+ next();
+});
 
 require('./routes')(App);
 
